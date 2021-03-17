7b38e8319aa40a21d4bd55c7291ac1fd6156b10e
German <delucagerman@gmail.com>
2021-03-17 11:50:44 -0300

    imagen url

--- components/intro.js
+++ components/intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
         G3rm@n<span className="w-5 h-5 md:w-10 md:h-10 bg-yellow-500 inline-block rounded-full mx-1 md:mx-5"></span> Blog.
       </h1>
       <h4 className="text-center text-petroleo md:text-left text-3xl mt-5 md:pl-8">
-        Dedicado a las tecnologias que me gustan y uso. Y articulos que me han ayudado a resolver problemas especificos de  manera facil + Programadores invitados a escribir{' '}
+        Dedicado a las tecnologias que me gustan y uso. Y artículos que me han ayudado a resolver problemas específicos de  manera fácil + Programadores invitados a escribir{' '}
         {/* <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
